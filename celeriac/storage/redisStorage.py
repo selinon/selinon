c9d944fed927229d515dbdb4c90bc4d8399efb2d
Fridolin Pokorny <fpokorny@unused-4-104.brq.redhat.com>
2016-08-29 10:27:04 +0200

    Introduce RedisStorage

--- celeriac/storage/redisStorage.py
+++ celeriac/storage/redisStorage.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python
 
+import redis
+import json
 from .dataStorage import DataStorage
 
 
 class RedisStorage(DataStorage):
-    def __init__(self):
+    def __init__(self, host="localhost", port=6379, db=0, password=None, socket_timeout=None, connection_pool=None,
+                 charset='utf-8', errors='strict', unix_socket_path=None):
         super(RedisStorage, self).__init__()
-        raise NotImplementedError()
+        self.conn = None
+        self.host = host
+        self.port = port
+        self.db = db
+        self.password = password
+        self.socket_timeout = socket_timeout
+        self.connection_pool = connection_pool
+        self.charset = charset
+        self.errors = errors
+        self.unix_socket_path = unix_socket_path
 
     def is_connected(self):
-        raise NotImplementedError()
+        return self.conn is not None
 
     def connect(self):
-        raise NotImplementedError()
+        self.conn = redis.Redis(host=self.host, port=self.port, db=self.db, password=self.password,
+                                socket_timeout=self.socket_timeout, connection_pool=self.connection_pool,
+                                charset=self.charset, errors=self.errors)
 
     def disconnect(self):
-        raise NotImplementedError()
+        self.conn.connection_pool.disconnect()
+        self.conn = None
 
     def retrieve(self, flow_name, task_name, task_id):
-        raise NotImplementedError()
+        ret = self.conn.get(task_id)
+
+        if ret is None:
+            raise FileNotFoundError("Record not found in database")
+
+        record = json.loads(ret.decode(self.charset))
+
+        assert(record.get('flow_name') == flow_name)
+        assert(record.get('task_name') == task_name)
+        assert(record.get('task_id') == task_id)
+
+        return record.get('result')
 
     def store(self, flow_name, task_name, task_id, result):
-        raise NotImplementedError()
+        assert(self.is_connected())
+
+        record = {
+            'flow_name': flow_name,
+            'task_name': task_name,
+            'task_id': task_id,
+            'result': result
+        }
+
+        self.conn.set(task_id, json.dumps(record))
 
