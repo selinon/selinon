15ae347896dffca80305818ab63e8018673fa996
Fridolin Pokorny <fpokorny@redhat.com>
2016-09-06 09:10:05 +0200

    Remove unused import

--- test/test_nodeFailures.py
+++ test/test_nodeFailures.py
@@ -19,7 +19,6 @@
 # ####################################################################
 
 import unittest
-import json
 
 from celeriac import FlowError
 from getTaskInstance import GetTaskInstance
