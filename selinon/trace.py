ed6174b2e5cdf2ffb2ac67c95aac8e800e231ff8
Fridolin Pokorny <fpokorny@redhat.com>
2016-10-04 11:14:37 +0200

    Warn about task retry as well in trace logs

--- selinon/trace.py
+++ selinon/trace.py
@@ -239,7 +239,8 @@ class Trace(object):
         """
         message = "%s: %s" % (cls.event2str(event), msg_dict)
 
-        if event == Trace.NODE_FAILURE or event == Trace.DISPATCHER_FAILURE or event == Trace.TASK_DISCARD_RESULT:
+        if event == Trace.NODE_FAILURE or event == Trace.DISPATCHER_FAILURE or event == Trace.TASK_DISCARD_RESULT \
+                or event == Trace.TASK_RETRY:
             return Trace._logger.warn(message)
         else:
             return Trace._logger.info(message)
