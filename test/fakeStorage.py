ef24ce723c081781d5367e1f1ecadcf6f85e6b35
Fridolin Pokorny <fpokorny@redhat.com>
2016-11-06 20:26:33 +0100

    Fix wrong import

--- test/fakeStorage.py
+++ test/fakeStorage.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # ####################################################################
 
-from selinonlib.storage import DataStorage
+from selinonlib.storages import DataStorage
 
 
 class FakeStorage(DataStorage):
