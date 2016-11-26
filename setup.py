ce2a3f6398823405ee3c62ec2452e28fd6bb6427
Fridolin Pokorny <fpokorny@redhat.com>
2016-11-26 15:49:10 +0100

    Centralize version info

--- setup.py
+++ setup.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 
 import sys
+import os
 from setuptools import setup
+
+
+NAME = 'selinon'
 
 
 def get_requirements():
     with open('requirements.txt') as fd:
         return fd.read().splitlines()
 
+
+def get_version():
+    with open(os.path.join(NAME, 'version.py')) as f:
+        version = f.readline()
+    return version.split(' = ')[1]
+
+
 if sys.version_info[0] != 3:
     sys.exit("Python3 is required in order to install selinon")
 
 setup(
-    name='selinon',
-    version='0.1.0rc3',
-    packages=['selinon'],
+    name=NAME,
+    version=get_version(),
+    packages=[NAME],
     install_requires=get_requirements(),
     author='Fridolin Pokorny',
     author_email='fpokorny@redhat.com',
     maintainer='Fridolin Pokorny',
     maintainer_email='fpokorny@redhat.com',
     description='task flow management for Celery',
-    url='https://github.com/fridex/selinon',
+    url='https://github.com/selinon/selinon',
     license='GPLv2+',
     keywords='celery selinonlib yaml condition flow',
     classifiers=[
