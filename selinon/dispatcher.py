c524b3abd6c81c9e9203816bb27728a272788154
Fridolin Pokorny <fpokorny@redhat.com>
2017-01-15 23:34:52 +0100

    Enrich dispatcher tracing points

--- selinon/dispatcher.py
+++ selinon/dispatcher.py
@@ -81,27 +81,30 @@ class Dispatcher(Task):
         :raises: FlowError
         """
 
-        Trace.log(Trace.DISPATCHER_WAKEUP, {'flow_name': flow_name,
-                                            'dispatcher_id': self.request.id,
-                                            'node_args': node_args,
-                                            'retry': retry,
-                                            'queue': Config.dispatcher_queues[flow_name],
-                                            'state': state})
+        Trace.log(Trace.DISPATCHER_WAKEUP, {
+            'flow_name': flow_name,
+            'dispatcher_id': self.request.id,
+            'node_args': node_args,
+            'retry': retry,
+            'queue': Config.dispatcher_queues[flow_name],
+            'state': state
+        })
         try:
             system_state = SystemState(self.request.id, flow_name, node_args, retry, state, parent)
             retry = system_state.update()
         except FlowError as flow_error:
             retried_count = retried_count or 0
             max_retry = Config.max_retry.get(flow_name, 0)
-
-            Trace.log(Trace.FLOW_FAILURE, {'flow_name': flow_name,
-                                           'dispatcher_id': self.request.id,
-                                           'state': json.loads(str(flow_error)),
-                                           'node_args': node_args,
-                                           'parent': parent,
-                                           'retry': retry,
-                                           'will_retry': retried_count < max_retry,
-                                           'queue': Config.dispatcher_queues[flow_name]})
+            Trace.log(Trace.FLOW_FAILURE, {
+                'flow_name': flow_name,
+                'dispatcher_id': self.request.id,
+                'state': json.loads(str(flow_error)),
+                'node_args': node_args,
+                'parent': parent,
+                'retry': retry,
+                'will_retry': retried_count < max_retry,
+                'queue': Config.dispatcher_queues[flow_name]
+            })
 
             if retried_count < max_retry:
                 raise self.selinon_retry(flow_name, node_args, parent, retried_count)
@@ -109,10 +112,14 @@ class Dispatcher(Task):
                 # force max_retries to 0 so we are not scheduled and marked as FAILED
                 raise self.retry(max_retries=0, exc=flow_error)
         except Exception as exc:
-            Trace.log(Trace.DISPATCHER_FAILURE, {'flow_name': flow_name,
-                                                 'dispatcher_id': self.request.id,
-                                                 'queue': Config.dispatcher_queues[flow_name],
-                                                 'what': traceback.format_exc()})
+            Trace.log(Trace.DISPATCHER_FAILURE, {
+                'flow_name': flow_name,
+                'dispatcher_id': self.request.id,
+                'node_args': node_args,
+                'parent': parent,
+                'queue': Config.dispatcher_queues[flow_name],
+                'what': traceback.format_exc()
+            })
             raise self.retry(max_retries=0, exc=exc)
 
         state_dict = system_state.to_dict()
@@ -137,10 +144,14 @@ class Dispatcher(Task):
             raise self.retry(args=[], kwargs=kwargs, countdown=retry, queue=Config.dispatcher_queues[flow_name])
         else:
             # TODO: make this more rich - same keys as FLOW_FAILURE
-            Trace.log(Trace.FLOW_END, {'flow_name': flow_name,
-                                       'dispatcher_id': self.request.id,
-                                       'queue': Config.dispatcher_queues[flow_name],
-                                       'finished_nodes': state_dict['finished_nodes']})
+            Trace.log(Trace.FLOW_END, {
+                'flow_name': flow_name,
+                'node_args': node_args,
+                'parent': parent,
+                'dispatcher_id': self.request.id,
+                'queue': Config.dispatcher_queues[flow_name],
+                'finished_nodes': state_dict['finished_nodes']
+            })
             return {
                 'finished_nodes': state_dict['finished_nodes'],
                 # this is always {} since we have finished, but leave it here because of failure tracking
