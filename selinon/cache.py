c69d2194c0c7e3aab2338e6039ef0b2b4d0395b0
Fridolin Pokorny <fridex.devel@gmail.com>
2017-04-18 16:59:35 +0200

    Fix docstrings

--- selinon/cache.py
+++ selinon/cache.py
@@ -4,21 +4,16 @@
 # Copyright (C) 2016-2017  Fridolin Pokorny, fridolin.pokorny@gmail.com
 # This file is part of Selinon project.
 # ######################################################################
-"""
-Base class for Cache interface
-"""
+""" Base class for Cache interface """
 
 import abc
 
 
 class Cache(metaclass=abc.ABCMeta):
-    """
-    Base class for Cache classes
-    """
+    """ Base class for Cache classes """
     @abc.abstractmethod
     def add(self, item_id, item, task_name=None, flow_name=None):
-        """
-        Add item to cache
+        """ Add item to cache
 
         :param item_id: item id under which item should be referenced
         :param item: item itself
@@ -29,8 +24,7 @@ class Cache(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def get(self, item_id, task_name=None, flow_name=None):
-        """
-        Get item from cache
+        """ Get item from cache
 
         :param item_id: item id under which the item is stored
         :param task_name: name of task that result should/shouldn't be cached, unused when caching Celery's AsyncResult
