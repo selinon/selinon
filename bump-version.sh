b8d049496c622b06fbeb70f3c1b97247dab51209
Fridolin Pokorny <fridolin@redhat.com>
2017-12-23 00:27:02 +0100

    Remove unused script

--- bump-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Generating new version identifier..."
-
-VERSION=`git describe --tags 2>/dev/null || git rev-parse --short HEAD`
-echo -e "selinon_version = '${VERSION}'" > selinon/version.py
-
-echo "Selinon version is '${VERSION}'"
