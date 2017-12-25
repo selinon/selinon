0f0688044bd857e210260a7076272e392db31f0f
Fridolin Pokorny <fridolin@redhat.com>
2017-12-26 00:10:42 +0100

    Flow definition option now accepts a path to directory

--- test/data/migrations/migration_dirs/_configuration/generate-migrations.sh
+++ test/data/migrations/migration_dirs/_configuration/generate-migrations.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-selinon-cli migrate -N ver_1/nodes.yml -F ver_1/flows/*.yml -n ver_2/nodes.yml -f ver_2/flows/*.yml --migration-dir .
-selinon-cli migrate -N ver_2/nodes.yml -F ver_2/flows/*.yml -n ver_3/nodes.yml -f ver_3/flows/*.yml --migration-dir .
+selinon-cli migrate -N ver_1/nodes.yml -F ver_1/flows/ -n ver_2/nodes.yml -f ver_2/flows/ --migration-dir .
+selinon-cli migrate -N ver_2/nodes.yml -F ver_2/flows/ -n ver_3/nodes.yml -f ver_3/flows/ --migration-dir .
